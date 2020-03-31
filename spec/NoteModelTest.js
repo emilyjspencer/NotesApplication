256539f90bd834b15f23946e2f48eb3aa379cc60
Emily <emilyspencer89@hotmail.co.uk>
2020-03-31 15:04:53 +0100

    Adds first test for Note Model

---
+++ spec/NoteModelTest.js
@@ -0,0 +1,13 @@
+
+(function NoteModelTest() {
+    var text = 'My favourite language is JavaScript'
+    var note = new Note(text)
+    
+    if(note.text !== text) {
+        throw new Error("Test failed")
+    } else {
+        console.log("Test passed")
+    }
+
+
+})();
